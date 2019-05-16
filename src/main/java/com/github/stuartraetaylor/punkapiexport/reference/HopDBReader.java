fb2019a5022535d433101906194605c27fc5e1cf
Stuart Taylor <stuartraetaylor@users.noreply.github.com>
2019-05-16 01:36:12 +0100

    Add hops extra dataset

--- src/main/java/com/github/stuartraetaylor/punkapiexport/reference/HopDBReader.java
+++ src/main/java/com/github/stuartraetaylor/punkapiexport/reference/HopDBReader.java
@@ -2,15 +2,31 @@ package com.github.stuartraetaylor.punkapiexport.reference;
 
 import java.io.File;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
-import com.github.stuartraetaylor.punkapiexport.JsonDBReader;
+import com.github.stuartraetaylor.punkapiexport.*;
 
-public class HopDBReader extends JsonDBReader<Hop> {
+public class HopDBReader implements Reader<Hop> {
 
-    private static final File baseDir = Paths.get("submodules/hops-json").toAbsolutePath().toFile();
+    private static final File baseDir = Paths.get("").toAbsolutePath().toFile();
 
-    public HopDBReader() {
-        super(baseDir, "hops-min.json", Hop.class);
+    @Override
+    public List<Hop> readAll() throws PunkException {
+        Reader<Hop> baseDb = new JsonDBReader<Hop>(baseDir, "submodules/hops-json/hops-min.json", Hop.class) { };
+        Reader<Hop> extrasDb = new JsonDBReader<Hop>(baseDir, "data/hops-extra.json", Hop.class) { };
+
+        return union(
+            baseDb.readAll(),
+            extrasDb.readAll()
+        );
+    }
+
+    private static <T> List<T> union(List<T> l1, List<T> l2) {
+        List<T> union = new ArrayList<>(l1.size() + l2.size());
+        union.addAll(l1);
+        union.addAll(l2);
+        return union;
     }
 
 }
